ecd6323f8e1d9dca3eb4b11c28f76f722b677773
Peter Igbafe <pigbafe@LNKLAP06200139.linkage.local>
2025-04-04 19:14:08 +0100

    feat: HomePage

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "./Footer.css";
+import { assets } from "../../assets/assets";
+
+const Footer = () => {
+  return (
+    <div className="footer" id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+            totam placeat ex, earum illo ad repellendus eveniet quis quod eos
+            cumque soluta quam suscipit dolorem fuga, excepturi architecto.
+            Modi, maiores.
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+1-234-334-5962</li>
+            <li>contact@tomato.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">
+        Copyright 2025 &copy; Tomato Delivery. All rights reserved.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
