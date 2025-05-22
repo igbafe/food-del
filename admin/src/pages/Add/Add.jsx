b602d7cda3c186beacd8f8c8fac47638bcd841b0
Igbafe Peter <igbafes8@gmail.com>
2025-05-22 13:22:09 +0100

    feat:order

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -99,7 +99,7 @@ const Add = ({url}) => {
             >
               <option value="Salad">Salad</option>
               <option value="Rolls">Rolls</option>
-              <option value="Dessert">Dessert</option>
+              <option value="Deserts">Deserts</option>
               <option value="Sandwich">Sandwich</option>
               <option value="Cake">Cake</option>
               <option value="Pure Veg">Pure Veg</option>
