9078d7d7ecd2c1465253527a6ff6165feeaa9efe
Igbafe Peter <igbafes8@gmail.com>
2025-05-31 22:51:42 +0100

    cors

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,11 @@ const port = process.env.PORT || 4000;
 
 // middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://food-del-frontend-2dwv.onrender.com", // your frontend URL
+  })
+);
 
 // db conection
 connectDB();
