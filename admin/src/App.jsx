24262d4c772c9294b6fac8404f4b75c25a4ea9ce
Igbafe Peter <62557429+igbafe@users.noreply.github.com>
2025-05-31 22:22:44 +0100

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -8,7 +8,7 @@ import List from "./pages/List/List";
 import { ToastContainer } from 'react-toastify';
 
 const App = () => {
-  const url = "http://localhost:4000";
+  const url = "https://food-del-backend-6llg.onrender.com";
   return (
     <div>
       <ToastContainer />
