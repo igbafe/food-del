9f778d3d8cf2b8ac9807d4b28bc0f95a1fdbd8dd
Igbafe Peter <igbafes8@gmail.com>
2025-06-21 16:26:37 +0100

    feat:hover

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -54,7 +54,7 @@ const Navbar = ({ setShowLogin }) => {
       <div className="navbar-right">
         <img src={assets.search_icon} alt="search" />
         <div className="navbar-search-icon">
-          <Link to="/cart">
+          <Link to="/cart" >
             <img src={assets.basket_icon} alt="" />
           </Link>
 
